dbfe7f9ea97057376526f58e6a7d876f2906b8c2
Handy <mvv0864@gmail.com>
2023-11-11 10:16:42 +0300

    Add project files.

---
+++ Module3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Victor";
+            Console.WriteLine(MyName);
+        }
+    }
+}
